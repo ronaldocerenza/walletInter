763296db995850bdebb14fe7011647f50e0bfc33
Ronaldo Cerenza <rockronald@gmail.com>
2023-05-12 16:39:58 -0400

    2 - Header para a página de carteira

---
+++ src/pages/Wallet.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+
+class Wallet extends React.Component {
+  render() {
+    return (
+      <div className="wallet-count">
+        <Header />
+      </div>
+
+    );
+  }
+}
+
+export default Wallet;
