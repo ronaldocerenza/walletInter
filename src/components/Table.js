152353805b8f963c514f19bfcda90a2ddd0466f0
Ronaldo Cerenza <rockronald@gmail.com>
2023-05-15 16:13:24 -0400

    7. tabela seja alimentada pelo estado 8 em constru

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,21 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deleteExpense, editExpense } from '../redux/actions';
 
 class Table extends Component {
+  handleClickDelete = (id) => {
+    const { dispatch } = this.props;
+    dispatch(deleteExpense(id));
+  };
+
+  handleClickEdit = (id) => {
+    const { dispatch } = this.props;
+    dispatch(editExpense(id));
+  };
+
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
@@ -18,10 +32,59 @@ class Table extends Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {expenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ Number(expense.value).toFixed(2) }</td>
+                <td>{ expense.exchangeRates[expense.currency].name }</td>
+                <td>
+                  { Number(expense.exchangeRates[expense.currency].ask)
+                    .toFixed(2) }
+                </td>
+                <td>
+                  {
+                    (Number(expense.value)
+                  * Number(expense.exchangeRates[expense.currency].ask)).toFixed(2)
+                  }
+                </td>
+                <td>Real</td>
+                <td>
+                  <button
+                    type="button"
+                    data-testid="edit-btn"
+                    onClick={ () => this.handleClickEdit(expense.id) }
+                  >
+                    Editar
+                  </button>
+                  <button
+                    type="button"
+                    data-testid="delete-btn"
+                    onClick={ () => this.handleClickDelete(expense.id) }
+                  >
+                    Excluir
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+    currency: PropTypes.string,
+  })).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
