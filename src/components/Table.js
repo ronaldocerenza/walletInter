32eb5e37456adbd9ce2c8466474f2eb9783e24d9
Ronaldo Cerenza <rockronald@gmail.com>
2023-05-15 18:17:49 -0400

    Update src/components/Table.js
    Ficou bem mais facil de ler o codico com poucas linhas
    
    Co-authored-by: João Felipe Zini <122232543+jfzini@users.noreply.github.com>

--- src/components/Table.js
+++ src/components/Table.js
@@ -22,15 +22,7 @@ class Table extends Component {
         <table>
           <thead>
             <tr>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar/Excluir</th>
+              {heads.map(head => <th key={head}>{head}</th>)}
             </tr>
           </thead>
           <tbody>
