51c1bdeba409542d0a539553d74a2a15dd9f6811
Ronaldo Cerenza <rockronald@gmail.com>
2023-10-16 15:57:37 -0400

    limpando arquivos trybe

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 
 ReactDOM
@@ -16,8 +15,3 @@ ReactDOM
       </BrowserRouter>
     </Provider>,
   );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
